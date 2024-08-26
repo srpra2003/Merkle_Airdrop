baa262e2da76af71a6630df57d568fbf14823ba4
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-08-27 00:36:28 +0530

    Created the TokenContract and Airdrop contract

---
+++ src/LaunchToken.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LaunchToken is ERC20, Ownable {
+    
+    constructor() ERC20("KillerToken", "KILL") Ownable(msg.sender){
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to,_amount);
+    }
+}
---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.26;
+
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+/**
+ * @title MerkeAirdrop
+ * @author Sohamkumar Prajapati
+ * @notice This is the contract for airdrop containg the list of the allowed address who
+ *         are given permission to get the airdrops from this contract
+ *         Simply this contract will provide ERC20 type tokens to allowed addresses
+ */
+
+contract MerkleAirdrop {
+
+    using SafeERC20 for IERC20;
+
+    error MerkeAirdrop__Airdrop_Already_Claimed();
+    error MerkeAirdrop__ProofInvalid();
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_AirdropToken;
+    mapping(address claimer => bool claimed) private claimed;
+
+    event Airdrop_Claimed(address indexed account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, address airdropToken){
+        i_merkleRoot = merkleRoot;
+        i_AirdropToken = IERC20(airdropToken);
+    }
+
+    function claim(address account, uint256 amount, bytes32[] memory proof) public {
+        if(claimed[account]){
+            revert MerkeAirdrop__Airdrop_Already_Claimed();
+        }
+
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account,amount))));
+
+        if(!MerkleProof.verify(proof, i_merkleRoot, leaf)){
+            revert MerkeAirdrop__ProofInvalid();
+        }
+
+        claimed[account] = true;
+        emit Airdrop_Claimed(account,amount);
+
+        i_AirdropToken.safeTransfer(account,amount);
+    }
+
+}
