13ee603d096a4fbb76ff5f284873bdd2da3f2a93
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-09-02 21:54:02 +0530

    All of the tests are created and tested successfully

--- test/unit/TestMerkleAirdrop.t.sol
+++ test/unit/TestMerkleAirdrop.t.sol
@@ -19,6 +19,7 @@ contract TestMerkleAirdrop is Test {
 
     address private USER = makeAddr("user");
     uint256 private INITIAL_BALANCE = 100 ether;
+    uint256 private AMOUNT_TO_CLAIM = 25 ether;
 
     event Airdrop_Claimed(address indexed account, uint256 amount);
 
@@ -36,7 +37,9 @@ contract TestMerkleAirdrop is Test {
     }
 
     function testInvalidAccountCannotCollectAirdrop() public {
-        address notValidPerson = makeAddr("invalidPerson");
+        (address notValidPerson, uint256 inValidPersonPrivateKey) = makeAddrAndKey("InvalidPerson");
+        bytes32 digest = merkleAirdrop.getMessageDigest(notValidPerson, AMOUNT_TO_CLAIM);
+        (uint8 _v, bytes32 _r, bytes32 _s) = signMessage(inValidPersonPrivateKey,digest);
 
         proof = vm.readFile(string.concat(vm.projectRoot(), "/script/target/output.json")).readBytes32Array("[0].proof");
 
@@ -46,31 +49,63 @@ contract TestMerkleAirdrop is Test {
 
         vm.startPrank(USER);
         vm.expectRevert(MerkleAirdrop.MerkeAirdrop__ProofInvalid.selector);
-        merkleAirdrop.claim(notValidPerson, 25e18, proof);
+        merkleAirdrop.claim(notValidPerson, 25e18, proof, _v, _r, _s);
     }
 
     function testValidUserCanCollectAirdrop() public {
-        address validUser =
-            vm.readFile(string.concat(vm.projectRoot(), "/script/target/output.json")).readAddress("[0].inputs[0]");
-        proof = vm.readFile(string.concat(vm.projectRoot(), "/script/target/output.json")).readBytes32Array("[0].proof");
-        console.log(validUser);
+        
+        address allowedUser = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json")).readAddress("[0].inputs[0]");
+        uint256 allowedUserPrivateKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
+        uint256 allowedUserInitialTokenBalance = launchToken.balanceOf(allowedUser);
+
+        proof = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json")).readBytes32Array("[0].proof");
+
+        bytes32 digest = merkleAirdrop.getMessageDigest(allowedUser,AMOUNT_TO_CLAIM);
+        (uint8 v, bytes32 r, bytes32 s) = signMessage(allowedUserPrivateKey,digest);
 
         vm.startPrank(USER);
-        vm.expectEmit(true, false, false, true, address(merkleAirdrop));
-        emit Airdrop_Claimed(validUser, 25e18);
-        merkleAirdrop.claim(validUser, 25e18, proof);
+        merkleAirdrop.claim(allowedUser,AMOUNT_TO_CLAIM,proof,v,r,s);
+        vm.stopPrank();
+
+        uint256 allowedUserFinalTokenBalance = launchToken.balanceOf(allowedUser);
+        assertEq(allowedUserInitialTokenBalance + AMOUNT_TO_CLAIM , allowedUserFinalTokenBalance);
     }
 
     function testAirdropscanOnlyBeClaimedOnce() public {
-        address validUser =
-            vm.readFile(string.concat(vm.projectRoot(), "/script/target/output.json")).readAddress("[0].inputs[0]");
-        proof = vm.readFile(string.concat(vm.projectRoot(), "/script/target/output.json")).readBytes32Array("[0].proof");
-        console.log(validUser);
+        address allowedUser = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json")).readAddress("[0].inputs[0]");
+        uint256 allowedUserPrivateKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
+
+        proof = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json")).readBytes32Array("[0].proof");
+
+        bytes32 digest = merkleAirdrop.getMessageDigest(allowedUser,AMOUNT_TO_CLAIM);
+        (uint8 v, bytes32 r, bytes32 s) = signMessage(allowedUserPrivateKey,digest);
 
         vm.startPrank(USER);
-        merkleAirdrop.claim(validUser, 25e18, proof);
+        merkleAirdrop.claim(allowedUser,AMOUNT_TO_CLAIM,proof,v,r,s);
+        vm.stopPrank();
 
         vm.expectRevert(MerkleAirdrop.MerkeAirdrop__Airdrop_Already_Claimed.selector);
-        merkleAirdrop.claim(validUser, 25e18, proof);
+        merkleAirdrop.claim(allowedUser,AMOUNT_TO_CLAIM,proof,v,r,s);
+    }
+
+    function testClaimFunctionrevertsWhenPassingWrongSignature() public {
+        address allowedUser = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json")).readAddress("[0].inputs[0]");
+        // uint256 allowedUserPrivateKey = vm.envUint("SEPOLIA_PRIVATE_KEY"); 
+        (,uint256 fakePrivateKey) = makeAddrAndKey("Fake Person");
+
+        proof = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json")).readBytes32Array("[0].proof");
+
+        bytes32 digest = merkleAirdrop.getMessageDigest(allowedUser,AMOUNT_TO_CLAIM);
+        (uint8 v, bytes32 r, bytes32 s) = signMessage(fakePrivateKey,digest);
+
+        vm.startPrank(USER);
+        vm.expectRevert(MerkleAirdrop.MerkleAirdrop__InvalidSignature.selector);
+        merkleAirdrop.claim(allowedUser,AMOUNT_TO_CLAIM,proof,v,r,s);
+        vm.stopPrank();
+    }
+
+    function signMessage(uint256 privateKey, bytes32 digest) public pure returns(uint8 v, bytes32 r, bytes32 s) {
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(privateKey, digest);
+        return (_v, _r, _s);
     }
 }
